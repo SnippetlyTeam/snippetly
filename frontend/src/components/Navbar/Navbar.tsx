391067da74ba9fb51036193032aa87120a97d3a1
Mykhailo <kurochkinmy@gmail.com>
2025-09-23 11:43:56 +0300

    feat: add navigation list and simple mixins

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import styles from './Navbar.module.scss';
 import Logo from '../Logo/Logo';
+import { NavLink } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.nav}>
       <Logo />
+
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.activeLink : ''}`
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className={styles.listItem}>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.activeLink : ''}`
+            }
+          >
+            Snippets
+          </NavLink>
+        </li>
+        <li className={styles.listItem}>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.activeLink : ''}`
+            }
+          >
+            About
+          </NavLink>
+        </li>
+        <li className={styles.listItem}>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.activeLink : ''}`
+            }
+          >
+            Log In
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
