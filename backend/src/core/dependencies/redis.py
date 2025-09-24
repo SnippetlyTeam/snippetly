64ec274d1aa23f40e52ecfd1a73e13921b256a4f
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2025-09-24 13:45:56 +0300

    add Redis async client, FastAPI dependency

---
+++ backend/src/core/dependencies/redis.py
@@ -0,0 +1,10 @@
+from fastapi.params import Depends
+from redis.asyncio import Redis
+
+from src.adapters.redis import get_redis_client as get_redis_adapter
+from src.core.config import Settings, get_settings
+
+
+async def get_redis_client(settings: Settings = Depends(get_settings())) -> Redis:
+    """FastAPI dependency for async Redis client"""
+    return get_redis_adapter(settings)
