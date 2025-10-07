ea54e924fd1a2d5e2ad5869c8235fe637db4d862
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2025-10-07 21:54:27 +0300

    create ProfileServiceInterface, define basic profiles routes

--- backend/src/api/v1/routes/profile.py
+++ backend/src/api/v1/routes/profile.py
@@ -1,3 +1,21 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/profiles", tags=["Profile Management"])
+from src.api.v1.schemas.common import MessageResponseSchema
+
+router = APIRouter(prefix="/profile", tags=["Profile Management"])
+
+
+@router.get("/", summary="Get user's profile details")
+async def get_profile_details(): ...
+
+
+@router.patch("/", summary="Update user's profile details")
+async def update_profile_details(): ...
+
+
+@router.post("/avatar", summary="Set user's profile avatar")
+async def set_profile_avatar() -> MessageResponseSchema: ...
+
+
+@router.delete("/avatar", summary="Delete user's profile avatar")
+async def delete_profile_avatar() -> MessageResponseSchema: ...
