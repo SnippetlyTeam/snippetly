9777b1476f6574621cb51b8e3f744cefbb89a0f1
Mykhailo <kurochkinmy@gmail.com>
2025-10-18 14:05:05 +0300

    feat: remove ability to change profile visibility

--- frontend/src/modules/ProfilePage/Settings.tsx
+++ frontend/src/modules/ProfilePage/Settings.tsx
@@ -5,15 +5,6 @@ const Settings = () => {
   return (
     <div className={styles.settings}>
       <h3 className={styles.settingsTitle}>Account Settings</h3>
-
-      <div className={styles.settingsItem}>
-        <span>Profile Visibility</span>
-
-        <select className={styles.select} defaultValue="public" aria-label="Profile visibility">
-          <option value="public">Public</option>
-          <option value="private">Private</option>
-        </select>
-      </div>
       <div className={styles.settingsItem}>
         <span>Change Password</span>
         <Link to='' className={styles.link}>Update Password</Link>
